672227add9119e1473dc1febbab1fde478087a0c
Kideri <kiderikun@gmail.com>
2019-10-22 19:05:13 +0300

    Fixed bug with config/core.py

---
+++ config/core.py
@@ -0,0 +1,11 @@
+from dialog_bot_sdk.bot import DialogBot
+import grpc
+
+token = 'f066f5dc53d0daf380533825fca65e67f6baebf1'
+endpoint = 'hackathon-mob.transmit.im'
+bot = DialogBot.get_secure_bot(
+    endpoint,
+    grpc.ssl_channel_credentials(),
+    token,
+    verbose=True
+)
